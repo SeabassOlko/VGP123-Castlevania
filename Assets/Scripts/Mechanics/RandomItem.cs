f92ce474b38d361fa8c5e6dfd128bd439c5bb0b6
SeabassOlko <63417711+SeabassOlko@users.noreply.github.com>
2024-05-14 15:34:59 -0500

    Implemented Candles to drop items / take collision from player weapons

--- Assets/Scripts/Mechanics/RandomItem.cs
+++ Assets/Scripts/Mechanics/RandomItem.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class RandomItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] public GameObject RedBag;
+    [SerializeField] public GameObject PurpleBag;
+    [SerializeField] public GameObject TanBag;
+    [SerializeField] public GameObject Heart;
+    [SerializeField] public GameObject Rosary;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Weapon") 
+        {
+            int ran = Random.Range(0, 5);
+            switch (ran)
+            {
+                case 0:
+                    Instantiate(RedBag, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+                case 1:
+                    Instantiate(PurpleBag, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+                case 2:
+                    Instantiate(TanBag, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+                case 3:
+                    Instantiate(Heart, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+                case 4:
+                    Instantiate(Rosary, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+            }
+            Destroy(gameObject);
+        }
     }
 }
